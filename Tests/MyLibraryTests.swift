1eb3444948f88fc23d5b7a0692e7c604f00b608a
CypherPoet <CypherPoet@gmail.com>
2021-02-24 23:56:34 -0600

    Remove nested starter test folder

--- Tests/MyLibraryTests.swift
+++ Tests/MyLibraryTests.swift
@@ -6,6 +6,6 @@ final class MyLibraryTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(MyLibrary().text, "Hello, World!")
+        XCTAssertEqual(1 + 1, 2)
     }
 }
