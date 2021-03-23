c1da5c4d6a0f15d4332ae700fc5acb1931aa8d9b
CypherPoet <CypherPoet@gmail.com>
2021-03-23 03:25:11 -0500

    make test throwing

--- Tests/MyLibrary/Cases/MyLibraryTests.swift
+++ Tests/MyLibrary/Cases/MyLibraryTests.swift
@@ -2,7 +2,7 @@ import XCTest
 @testable import MyLibrary
 
 final class MyLibraryTests: XCTestCase {
-    func testExample() {
+    func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
