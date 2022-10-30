d3c5827ea0c0e9b864d7d9f09d86eef62610dbbb
CypherPoet <CypherPoet@tutanota.com>
2022-10-30 06:30:55 -0500

    Update starter unit testing file
    Support Async/Await and add some default hooks and cases.

--- Tests/MyLibraryName/Cases/MyLibraryNameTests.swift
+++ Tests/MyLibraryName/Cases/MyLibraryNameTests.swift
@@ -1,11 +1,83 @@
 import XCTest
 import MyLibraryName
 
+
 final class MyLibraryNameTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(MyLibraryName().text, "Hello, World!")
+    internal typealias SystemUnderTest = MyLibraryName
+
+    internal var sut: SystemUnderTest!
+}
+
+
+// MARK: - Lifecycle
+extension MyLibraryNameTests {
+
+    override func setUp() async throws {
+        // Put setup code here.
+        // This method is called before the invocation of each
+        // test method in the class.
+
+        try await super.setUp()
+
+        sut = makeSUT()
+    }
+
+
+    override func tearDown() async throws {
+        // Put teardown code here.
+        // This method is called after the invocation of each
+        // test method in the class.
+
+        try await super.tearDown()
+
+        sut = nil
+    }
+}
+
+
+// MARK: - Factories
+extension MyLibraryNameTests {
+
+    internal func makeSUT() -> SystemUnderTest {
+        .init(
+            // 💡 Pass in arguments by using current properties
+            // of the test class instance.
+        )
+    }
+
+    /// Helper to make the system under test from any default initializer
+    /// and then test its initial conditions
+    internal func makeSUTFromDefaults() -> SystemUnderTest {
+        .init()
+    }
+}
+
+
+// MARK: - "Given" Helpers (Conditions Exist)
+extension MyLibraryNameTests {
+
+    internal func givenSomething() {
+        // some state or condition is established
+    }
+}
+
+
+// MARK: - "When" Helpers (Actions Are Performed)
+extension MyLibraryNameTests {
+
+    internal func whenSomethingHappens() {
+        // perform some action
+    }
+}
+
+
+// MARK: - Test - Init with Default Properties
+extension MyLibraryNameTests {
+
+    func test_Init_WhenCreatingWithDefaultProperties_ItSetsTextToExpectedValues() async throws {
+        let expected = "Hello, World!"
+        let actual = sut.text
+
+        XCTAssertEqual(actual, expected)
     }
 }
